389b8af48ad240dc2a2d13bb0e8bd8b4e9d8cab7
kaysee <kassandra.orozco@gmail.com>
2016-11-17 10:55:19 -0500

    ideas added

--- FinalProject.hs
+++ FinalProject.hs
@@ -8,6 +8,10 @@
 -- $ git commit -m "commit description here, what changes were made?"
 -- $ git push, pushes changes to git so we both see them
 
+-- COMPILER STEPS:
+-- $ ghc --make FinalProject, first time only
+-- $ ./FinalProject
+
 data Players = X
              | O
              | E
@@ -16,4 +20,17 @@ data Players = X
 board :: [Players] -> [Players]
 board = undefined
 
--- hello kaysee
+main :: IO ()
+main = do
+     putStrLn "What game do you want to play? A: TicTacToe, B: Connect Four, C: Checkers, D: Chess"
+     gameChoice <- getLine
+     putStrLn $ "Let's begin!"
+
+-- Next Steps:
+-- create initial board
+-- can main take helper functions? how does main work?
+-- make function that modifies the board every time a move is made
+-- function that checks after each move if there is a winner
+-- can we loop in main?
+-- make the board a list? a list of lists?
+-- how to associate grid with actual spot, so A2 with bottom middle square

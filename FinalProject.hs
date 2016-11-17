a00595d367a4a1f1fbf0dc41bef7ddd0f19db43e
kaysee <kassandra.orozco@gmail.com>
2016-11-16 21:47:25 -0500

    git directions

--- FinalProject.hs
+++ FinalProject.hs
@@ -1,5 +1,13 @@
 -- Final Project
 
+-- GIT STEPS:
+-- make sure you're in the correct directory in your terminal
+-- $ git pull, pulls changes made (by other person)
+-- $ git status, checks to see what files have changed
+-- $ git add FinalProject.hs
+-- $ git commit -m "commit description here, what changes were made?"
+-- $ git push, pushes changes to git so we both see them
+
 data Players = X
              | O
              | E
